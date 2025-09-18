3e615dedbe57beed1b5f12219ca41d194ce29a29
Shivani Chaurasiya <shivanichaurasiya57132@gmail.com>
2025-09-18 23:02:29 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+// const accessKey = 'BFkaCUyh7KFQ5xRSq-rxs5Z71DGu5DAf-Oc-bdt9Kq8'; // Replace with your Unsplash Access Key
+
+    async function searchImages() {
+      const query = document.getElementById('search-input').value.trim();
+      const imageContainer = document.getElementById('image-container');
+      imageContainer.innerHTML = ''; // Clear previous results
+
+      if (!query) {
+        alert("Please enter a keyword.");
+        return;
+      }
+
+      try {
+        const response = await fetch(`https://searchimageappbackend.onrender.com/api/search-images?query=${query}`);
+        const data = await response.json();
+
+        if (data.results.length > 0) {
+          data.results.forEach(photo => {
+            const img = document.createElement('img');
+            img.src = photo.urls.regular;
+            img.alt = photo.alt_description || 'Unsplash Image';
+
+            const div = document.createElement('div');
+            div.className = 'image-item';
+            div.appendChild(img);
+
+            imageContainer.appendChild(div);
+          });
+        } else {
+          alert("No images found. Try another keyword.");
+        }
+      } catch (error) {
+        console.error('Error fetching images:', error);
+        alert("Something went wrong. Try again.");
+      }
+    }
+    //https://api.unsplash.com/search/photos?query=${query}&client_id=${accessKey}&per_page=12
